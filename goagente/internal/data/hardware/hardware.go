362c2a66adb1ae2a5649286c3b807d4e88c36ebe
Arthurzineo1 <101883886+Arthurzineo1@users.noreply.github.com>
2024-09-12 21:31:21 -0300

    Delete goagente/internal/data/hardware/hardware.go

--- goagente/internal/data/hardware/hardware.go
+++
@@ -1,9 +0,0 @@
-package hardware
-
-// HardwareInfoFactory define os métodos para obter informações de hardware
-type HardwareInfoFactory interface {
-	GetDiskInfo() ([]DiskInfo, error)
-	GetMotherboardInfo() (MotherboardInfo, error)
-	GetProcessorInfo() ([]ProcessorInfo, error)
-	GetRAMInfo() ([]RAM, error)
-}
